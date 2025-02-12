db539a7680d7c4292b3ce2eb93eeadc2597bee0b
RoxBlox3 <RoxBlox33@proton.me>
2025-02-12 10:29:32 +0100

    Added profiler

--- config/.config/nvim/lua/config/keymaps.lua
+++ config/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+Snacks.toggle.profiler():map("<leader>pp")
+Snacks.toggle.profiler_highlights():map("<leader>ph")
