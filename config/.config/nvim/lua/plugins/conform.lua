ee5e4955bea0eba67b6f3fbba8926ac13af0745b
RoxBlox3 <RoxBlox33@proton.me>
2025-03-04 13:49:58 +0100

    Configured Prettier

---
+++ config/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,26 @@
+return {
+	"steavearc/conform.nvim",
+	opts = {
+		formatters = {
+			prettier = {
+				prepend_args = {
+					"--tab-width",
+					"4",
+				},
+			},
+		},
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			html = { "prettier" },
+			css = { "prettier" },
+			scss = { "prettier" },
+			markdown = { "prettier" },
+			lua = { "stylua" }, -- Example for Lua
+		},
+	},
+}
